0f5e654a33508d56062527abd4f821b7be41040d
Ahmed <ahmedwebdeveloper124@gmail.com>
2026-06-28 12:17:33 +0100

    added compare program (new)

---
+++ week 1/compare/comapre.c
@@ -0,0 +1,22 @@
+// this 'compare' program!
+#include "../cs50.h"
+#include <stdio.h>
+
+int main(void)
+{
+    int x = get_int("What's x ? ");
+    int y = get_int("what's y ? ");
+
+    if (x > y)
+    {
+        printf("x is greater than y.");
+    }
+    else if (x < y)
+    {
+        printf("x is less than y.");
+    }
+    else
+    {
+        printf("x is equal to y.");
+    }
+}
